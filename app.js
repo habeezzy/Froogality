85b13adaa2340d556b04b67fe78a8291ea7a24f3
habeezzy <mhabeebulah@gmail.com>
2019-07-17 16:28:23 -0400

    Global app controller functionality

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ var budgetController = (function() {
 })();
 
 
+
+
 //UI CONTROLLER
 
 var UIController = (function() {
@@ -23,12 +25,35 @@ var UIController = (function() {
 })();
 
 
+
+
+
 // GLOBAL APP CONTROLLER
 
 var controller = (function(budgetCtrl, UICtrl) {
 
-  document.querySelector(".add__btn").addEventListener("click", function() {
-    console.log("button was clicked");
+  var ctrlAddItem = function() {
+
+        // 1. Get the filled input data
+
+        //2. Add the item to the budget CONTROLLER
+
+        //3. Add the item to the UI
+
+        //4. Calculate the  budget
+
+        //5. Display the budget on the UI
+        console.log("It works");
+
+  }
+
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+
+  document.addEventListener("keypress", function(event) {
+
+    if(event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
   });
 
 })(budgetController, UIController);
